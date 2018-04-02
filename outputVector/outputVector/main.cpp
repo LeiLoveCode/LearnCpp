271c1cf48611345e3e986837c35ae3f86c0329fd
lei <chenlei_93@hotmail.com>
2018-04-02 21:49:10 +0800

    test

--- outputVector/outputVector/main.cpp
+++ outputVector/outputVector/main.cpp
@@ -10,12 +10,12 @@ void main()
 {
 	//输出double类型的vector
 	//新建double类型的容器，名称Output，5个数字，都为4
-	std::vector<double> Output(5,4);
+//	std::vector<double> Output(5,4);
 	//输出
-	OutputVector(Output);
+//	OutputVector(Output);
 	
 	//vector的基本使用
-	BasicVector();
+//	BasicVector();
 
 	//定义了一个f=x+y的函数，计算20*20范围的值，间隔1
 	GridData(1, 1, 1, 1, 10, 1);
