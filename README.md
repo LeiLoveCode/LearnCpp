595bf5846a12cb16edcb8458292e23250689029a
lei <chenlei_93@hotmail.com>
2018-05-17 21:50:00 +0800

    文件的输入输出
    1.根据给定的文件目录按行读取并输出到控制台

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 cpp学习记录
 
-### vector的使用
+## vector的使用
 
 1.vector的基本用法       vector.cpp
 
 2.vector的基本输出        OutputVector.cpp
 
 3.使用vector输出函数的网格化结果       GridData.cpp
+
+## 文件的读取和写入
+
+1.根据给定的文件目录按行读取并输出到控制台 		txtin.cpp
---
+++ TXTin/TXTin/txtin.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+
+double txtin(std::string path)
+{
+	//打开"path"下文件
+	std::ifstream ContentIn(path);
+	if (!ContentIn.is_open())
+	{
+		//如果文件打开失败
+		std::cout << "error open txt:" << path << std::endl;
+		//返回0
+		return 0;
+
+	}
+
+	//定义一个string类型变量用于存储读取的行
+	std::string temp;
+
+	//如果行不为空，循环读取
+	while (std::getline(ContentIn,temp))
+	{
+		//输出该行数据
+		std::cout << temp << std::endl;
+
+	}
+
+	//数据读取完毕，关闭文件
+	ContentIn.close();
+
+	//返回1
+	return 1;
+
+}
+
+
+
+
+
+void main()
+{
+
+	std::string testPath = "in\\test.txt";
+	txtin(testPath);
+
+	system("pause");
+
+}
